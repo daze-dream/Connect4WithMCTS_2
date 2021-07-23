06d0635b1dd7fbc69a4eb2919297cd981b1f5f87
CheetoDustNeo <misterrolypoly@gmail.com>
2021-07-22 23:12:50 -0500

    Added win checking functions, moved input verification to separate function in GameBoard class.

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -11,6 +11,16 @@ GameBoard::~GameBoard()
 {
 }
 
+bool GameBoard::isValidColumn(char pos)
+{
+	
+	if (!isdigit(pos))
+		return false;
+	if (pos - '0' > column || pos - '0' <= 0)
+		return false;
+	return true;
+}
+
 void GameBoard::displayBoard()
 {
 	cout << tab
@@ -33,9 +43,13 @@ void GameBoard::displayBoard()
 
 bool GameBoard::doMoveAndChangePlayer(int pos)
 {
-	int realPos = pos - 1;
-	if (placePiece(realPos))
+	//means the index in the vector, as opposed to the real column value
+	int indexPos = pos - 1;
+	pair<int, bool> result = placePiece(indexPos);
+	if (result.second)
 	{
+		//checks if game is over, and changes terminal bool
+		terminal = checkWinAtPos(result.first, indexPos);
 		switchActivePlayer();
 		moves++;
 		return true;
@@ -46,43 +60,107 @@ bool GameBoard::doMoveAndChangePlayer(int pos)
 	}
 }
 
-bool GameBoard::placePiece(int pos)
+pair<int, bool> GameBoard::placePiece(int pos)
 {
 	bool success = false, turnOver = false;
 	int depth = row - 1 ;
+	pair<int, bool> toReturn = make_pair(0, false);
 	for (int i = depth; i >= 0; i--)
 	{
 		if (board[i][pos] == 0)
 		{
 			board[i][pos] = playerID;
 			success = true;
+			toReturn.first = i;
+			toReturn.second = success;
 			break;
 		}
 	}
-	return success;
+
+	return toReturn;
+}
+
+void GameBoard::placePieceRaw(int row, int column, int id)
+{
+	board[row][column] = id;
+}
+
+bool GameBoard::isLinearMatch(int r, int c, int stepX, int stepY)
+{
+	const int startValue = this->board[r][c];
+	bool is4Match = true;
+
+	/* Confirm the two values after it match. */
+	for (int i = 1; i < 4; ++i) // forward and downward
+	{
+		int columnStep = c + i * stepX;
+		int rowStep = r + i * stepY;
+		//cout << "row " << rowStep + 1 << " column " << columnStep + 1 << ": ";
+		if (rowStep > 5 || rowStep < 0)
+		{
+			//cout << "row out of range";
+			return false;
+		}
+		if (columnStep > 6 || columnStep < 0)
+		{
+			//cout << "column out of range";
+			return false;
+		}
+		//cout << this->board[rowStep][columnStep] << " ";
+		if (this->board[rowStep][columnStep] != startValue)
+		{
+			is4Match = false;
+			break;
+		}
+	}
+	/* If we got here, then they all match! */
+	return is4Match;
+
+}
+
+bool GameBoard::checkWinAtPos(int r, int c)
+{
+	return (isLinearMatch(r, c, 1, 0) ||  // Horizontal forward
+		isLinearMatch(r, c, -1, 0) || // Horizontal backward
+		isLinearMatch(r, c, 0, -1) ||	//Vertical up
+		isLinearMatch(r, c, 0, 1) ||  // Vertical down
+		isLinearMatch(r, c, 1, 1) ||  // diagonal forward->down
+		isLinearMatch(r, c, -1, -1) || //diagonal back->up
+		isLinearMatch(r, c, 1, -1) || // diagonal forward->up
+		isLinearMatch(r, c, -1, 1)); //diagonal back->down
 }
 
 void GameBoard::startGame()
 {
 	displayBoard();
-	while (moves < 42)
+	while (!terminal)
 	{
+		char input = ' ';
 		int pos = 0;
 		cout << "Enter column: ";
-		while (!(cin >>pos) && pos > column) {
+		cin >> input;
+		while (!isValidColumn(input)) 
+		{
+			cout << "Invalid input.  Try again: ";
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << "Invalid input.  Try again: ";
+			cin >> input;
 		}
+		pos = input - '0';
 		cout << "Dropping at: " << pos << "...";
 		if (doMoveAndChangePlayer(pos))
 		{
 			cout << "success!" << endl;
+
 		}
 		else
 		{
 			cout << pos << " is full. Select another column." << endl;
 		}
 		displayBoard();
+	}
+	if (terminal && !draw)
+	{
+		cout << "Player " << getOpponentID() << " won in " << moves << " moves!" << endl;
 	}
 }
--- GameBoard.h
+++ GameBoard.h
@@ -2,6 +2,7 @@
 #include <array>
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 class GameBoard
@@ -10,7 +11,12 @@ public:
 	GameBoard(int r, int c);
 	~GameBoard();
 	int getPlayerID() { return this->playerID; };
+	int getOpponentID() { return 3 - playerID; };
+	bool getIfGameIsOver() { return terminal; };
+	bool getIfGameIsDraw() { return draw; };
+	bool getIfGameIsOverAndDraw() { return (terminal && draw); };
 	auto getBoard() { return this->board; };
+	bool isValidColumn(char pos);
 	//display board to console
 	void displayBoard();
 	// master game manager to be called from main.
@@ -19,11 +25,22 @@ public:
 	//these should be private later
 
 	void switchActivePlayer() { playerID = 3 - playerID; };
-	// to do all parts of executing a move: place piece, check win, status, AI move, etc.
+	/// <summary>
+	/// Currently checks validity, places a piece, checks win condition
+	/// </summary>
+	/// <param name="pos">: the raw row value. Converts into proper index inside function</param>
+	/// <returns></returns>
 	bool doMoveAndChangePlayer(int pos);
-	// to only place piece at position. doMove handles the bounds checking for column.
-	bool placePiece(int pos);
-
+	/// <summary>
+	/// used to place a piece at the selected column position only.
+	/// </summary>
+	/// <param name="pos">: the index of the vector. Higher level functions change real column to index value.</param>
+	/// <returns>a pair: first value is the depth, second value is bool for successful placement</returns>
+	pair<int, bool> placePiece(int pos);
+	// for testing purposes, places a value at any part of th 2D array.
+	void placePieceRaw(int row, int column, int id);
+	bool isLinearMatch(int r, int c, int stepX, int stepY);
+	bool checkWinAtPos(int r, int c);
 private:
 	//maxmium moves in a game 
 	const int maxMoves = 42;
@@ -37,9 +54,7 @@ private:
 	bool terminal = false;
 	// to track how many moves have been made
 	int moves = 0;
-	// checking to see if there is a win
-	//bool IsLinearMatch(int column, int row, int stepX, int stepY);
-	
+	// checking to see if there is a win	
 
 	string tab = "                         ";
 
