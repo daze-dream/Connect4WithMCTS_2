630335b6ade0ad1c71e164a6eac0cbbb60c4cfcf
CheetoDustNeo <misterrolypoly@gmail.com>
2021-07-25 00:52:04 -0500

    new class. added all possible states functionality

--- GameBoardMCTS.h
+++ GameBoardMCTS.h
@@ -5,7 +5,9 @@ class GameBoardMCTS :
     public GameBoard
 {
 public:
+    GameBoardMCTS() : GameBoard() {};
     GameBoardMCTS(int r, int c) : GameBoard(r, c) {};
+    GameBoardMCTS(const GameBoardMCTS&);
     // returns possible moves, aka columns that aren't filled up
     vector<int> possibleMoves();
 
