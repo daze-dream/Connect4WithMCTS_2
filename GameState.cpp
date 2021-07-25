1413ef5b9e59e86cfc4222134cd195ede503d740
CheetoDustNeo <misterrolypoly@gmail.com>
2021-07-25 00:55:18 -0500

    new class. added all possible states functionality

---
+++ GameState.cpp
@@ -0,0 +1,62 @@
+#include "GameState.h"
+
+GameState::GameState(GameBoardMCTS incBoard)
+{
+	this->stateBoard = incBoard;
+}
+
+bool GameState::doMoveForBoard(int pos)
+{
+	return this->stateBoard.doMoveAndChangePlayer(pos);
+}
+
+vector<GameState> GameState::getPossibleStates()
+{
+	return possibleStates;
+}
+
+void GameState::fillUpPossibleStates()
+{
+	vector<int> movesToDo = stateBoard.possibleMoves();
+	// needed because when we push directly to the state vector, each new state was getting a copy as well.
+	vector<GameState> tempStates;
+	while (!movesToDo.empty())
+	{
+		GameState newState = *this;
+		newState.getBoard().doMoveAndChangePlayer(movesToDo.back() + 1);
+		//if the move actually works aka column not filled...
+		if (newState.doMoveForBoard(movesToDo.back() + 1))
+		{
+			//then push back that state.
+			tempStates.push_back(newState);
+		}
+		movesToDo.pop_back();
+	}
+	possibleStates = tempStates;
+
+}
+
+vector<GameState> GameState::fillThenReturnPossibleStates()
+{
+	vector<int> movesToDo = stateBoard.possibleMoves();
+	// needed because when we push directly to the state vector, each new state was getting a copy as well.
+	vector<GameState> tempStates;
+	while (!movesToDo.empty())
+	{
+		GameState newState = *this;
+		newState.getBoard().doMoveAndChangePlayer(movesToDo.back() + 1);
+		//if the move actually works aka column not filled...
+		if (newState.doMoveForBoard(movesToDo.back() + 1))
+		{
+			//then push back that state.
+			tempStates.push_back(newState);
+		}
+		movesToDo.pop_back();
+	}
+	possibleStates = tempStates;
+	return possibleStates;
+}
+
+GameState::~GameState()
+{
+}
---
+++ GameState.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "GameBoardMCTS.h"
+#include <vector>
+
+
+
+class GameState
+{
+public: 
+	//constructor that takes a gameboard. default constructor taken care of in base class
+	GameState(GameBoardMCTS incBoard);
+	void setBoard(GameBoardMCTS incBoard) { stateBoard = incBoard; };
+	auto getBoard() { return stateBoard; };
+	// simple getter, does not fill up states. See fillUpPossibleStates
+	vector<GameState> getPossibleStates();
+	// much safer than returning board by reference. does a move in the internal board
+	bool doMoveForBoard(int);
+	int getVisitCount() { return visitCount; };
+	double getWinScore() { return winScore; };
+	// fills up the internal possibleStates vector
+	void fillUpPossibleStates();
+	// fills up and then returns the possibleStates vector.
+	vector<GameState> fillThenReturnPossibleStates();
+
+
+
+
+	~GameState();
+private:
+	GameBoardMCTS stateBoard;
+	int visitCount = 0;
+	double winScore = 0;
+	vector<GameState> possibleStates;
+};
+
